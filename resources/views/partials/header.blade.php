e86c3152b084862867632ea956d86ae3cabb41ca
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-21 16:07:06 +0100

    Add: content in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,27 @@
 <header>
     <div class="container">
-      {{-- <img src="../assets/img/dc-logo.png" alt="logo DC comics"> --}}
-      <ul>
-          <li>
-              <a href="link.href" class="active">
-
-              </a>
-          </li>
-      </ul>
+        {{-- <img src="../assets/img/dc-logo.png" alt="logo DC comics"> --}}
+        <ul>
+            <li>
+                <a href="link.href" class="active">
+                    characters
+                </a>
+            </li>
+            <li>
+                <a href="link.href" class="active">
+                    comics
+                </a>
+            </li>
+            <li>
+                <a href="link.href" class="active">
+                    movies
+                </a>
+            </li>
+            <li>
+                <a href="link.href" class="active">
+                    tv
+                </a>
+            </li>
+        </ul>
     </div>
-  </header>
 </header>
