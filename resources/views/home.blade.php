5f635fd821350d28458452964ed2999dc74edc28
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-21 14:39:46 +0100

    Add: content in Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Hello world</h1>
+    <h1>Hello world Home</h1>
+    <a href="test">TEST</a>
 @endsection
---
+++ resources/views/test.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1>Hello world Test</h1>
+    <a href="/">HOME</a>
+@endsection
