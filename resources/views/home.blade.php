d2b61fdafa5557a483cf5a2f9bea82bc3eb1cd35
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-21 15:41:51 +0100

    Add: cards section in Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,19 @@
 @section('content')
     <h1>Hello world Home</h1>
     <a href="test">TEST</a>
+    <section id="cards">
+        <div class="container">
+            <h2>
+            CURRENT SERIES 
+            </h2>
+            <div class="cards_container">
+                
+            </div>
+            <h5>
+                <a href="#">
+                    LOAD MORE
+                </a>
+            </h5>
+        </div>
+    </section>
 @endsection
