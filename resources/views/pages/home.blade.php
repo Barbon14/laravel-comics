4fdfa9a1b018bf307f9cfe019c91d987eb5cba5d
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-24 15:29:47 +0100

    Add: comics in Home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,16 @@
             CURRENT SERIES 
             </h2>
             <div class="cards_container">
-                
+                @foreach ($comics as $comic)
+                    <div class="card">
+                        <a href="{{ route('test') }}">
+                            <img src="{{ $comic["thumb"] }}" :alt="{{ $comic["series"] }}">
+                            <h4>
+                                {{ $comic["series"] }}
+                            </h4>
+                        </a>
+                    </div>
+                @endforeach
             </div>
             <h5>
                 <a href="#">
