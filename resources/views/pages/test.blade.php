57a436efa2fbdfc7d033c7f9072211faeed00e44
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-24 15:42:02 +0100

    Add: content in Test

--- resources/views/pages/test.blade.php
+++ resources/views/pages/test.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Hello world Test</h1>
-    <a href="{{ route('home') }}">HOME</a>
+    <section class="container">
+        <h2>action comics #1000: the deluxe edition</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis non dolorum id reiciendis dolore, perspiciatis incidunt nobis. Error facilis, vero dolorum, iste beatae cumque iure quia alias veniam, quaerat quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptates recusandae animi iste voluptatem aperiam quos corrupti laudantium laboriosam necessitatibus libero possimus tenetur nostrum corporis, soluta sequi saepe autem fuga?</p>
+        <a href="{{ route('home') }}">HOME</a>
+    </section>
 @endsection
